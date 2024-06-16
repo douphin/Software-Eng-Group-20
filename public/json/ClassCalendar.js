50516863cf05b3b3ff676ba8b9ada1904ee48ea9
Douglas Phinney <dphinney777@gmail.com>
2024-06-16 17:16:46 -0400

    Adding Example Classes

---
+++ public/json/ClassCalendar.js
@@ -0,0 +1,34 @@
+class Calendar{
+    Calendar(){}
+
+    LoadJSON(){}
+    UpdateJSON(){}
+
+    LoadEvent(){}
+    NewEvent(){}
+
+    CurrentMonth;
+    ArrHoliday;
+
+}
+
+class Weather{
+    Weather(){}
+
+    GetWeatherFull(){}
+    GetCalendarWeather(){}
+    GetPartialWeather(){}
+
+    WeatherJSON;
+}
+
+class Profile{
+    Profile(){}
+
+    CheckLogin(){}
+    CreateNewProfile(){}
+    LoadProfile(){}
+
+    CurrentProfile;
+
+}
